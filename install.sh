68054e47d86c96d26a436a01043a4e61034ebbda
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-04 19:08:12 -0700

    adds pips and spacing

--- install.sh
+++ install.sh
@@ -1,18 +1,39 @@
 # Startup info
 echo "Installing GLauncher"
 echo "Project can be found here: https://github.com/HttpAnimation/GLauncher"
+echo ""
+echo ""
+echo ""
 
 # Git clone the branch stable
+clear
+echo "Installing GLauncher"
+echo "Project can be found here: https://github.com/HttpAnimation/GLauncher"
+echo ""
+echo ""
+echo ""
 echo "Downloading the repo"
 git clone -b https://github.com/HttpAnimation/GLauncher
 echo "Done downloading the repo"
 
 # Cd the repo
+clear
+echo "Installing GLauncher"
+echo "Project can be found here: https://github.com/HttpAnimation/GLauncher"
+echo ""
+echo ""
+echo ""
 echo "Heading into the repo"
 cd GLauncher
 echo "Done heading into the repo"
 
 # Remove extra files
+clear
+echo "Installing GLauncher"
+echo "Project can be found here: https://github.com/HttpAnimation/GLauncher"
+echo ""
+echo ""
+echo ""
 echo "Removing extra / unsed files"
 echo "Removing README.md"
 rm README.md
@@ -20,3 +41,12 @@ echo "Done removing the readme"
 echo "Removes configs/README.md"
 rm configs/README.md
 echo "Removing configs/README.md"
+echo "Done removing files"
+
+# Download with pip
+echo "Installing GLauncher"
+echo "Project can be found here: https://github.com/HttpAnimation/GLauncher"
+echo ""
+echo ""
+echo ""
+echo "Now downloading pip's"

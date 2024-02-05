2b92809e9abc34f6b2c7033b097ab435369bdd72
HttpAnimation <shawndouglasyt@gmail.com>
2024-02-04 18:26:48 -0700

    adds a testing window

--- Pre-1.1/github.httpanimations.glauncher/src/GLauncher.java
+++ Pre-1.1/github.httpanimations.glauncher/src/GLauncher.java
@@ -1,5 +1,30 @@
-public class GLauncher {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class GLauncher extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Create a label with the text "Test"
+        Label label = new Label("Test");
+
+        // Create a scene with the label
+        Scene scene = new Scene(label, 200, 100);
+
+        // Set the scene to the primary stage
+        primaryStage.setScene(scene);
+
+        // Set the title of the window
+        primaryStage.setTitle("Test Window");
+
+        // Show the window
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        // Launch the JavaFX application
+        launch(args);
     }
 }
